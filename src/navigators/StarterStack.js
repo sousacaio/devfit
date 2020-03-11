1ec8f43956aa45a1d55c69aeca6a8ba61f09418f
Shouganaii <frias.caio@yahoo.com.br>
2020-03-11 01:12:28 -0300

    Primeiro commit:Starter intro

---
+++ src/navigators/StarterStack.js
@@ -0,0 +1,11 @@
+import { createStackNavigator } from 'react-navigation-stack';
+
+import StarterIntro from '../screens/StarterIntro';
+
+export default createStackNavigator({
+    StarterIntro
+}, {
+    defaultNavigationOptions: {
+        headerShown: false
+    }
+});
